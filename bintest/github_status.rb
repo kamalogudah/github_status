e6edf9e94a7fa13b9f46aa77e9b276a117c576b2
Christophe Philemotte <christophe.philemotte@8thcolor.com>
2016-03-28 23:02:07 +0200

    Add a simple integration test

--- bintest/github_status.rb
+++ bintest/github_status.rb
@@ -2,14 +2,14 @@ require 'open3'
 
 BIN_PATH = File.join(File.dirname(__FILE__), "../mruby/bin/github_status")
 
-assert('hello') do
-  output, status = Open3.capture2(BIN_PATH)
+assert('It tells you it\'s not a Github repo') do
+  output, status = Open3.capture2e(BIN_PATH)
 
-  assert_true status.success?, "Process did not exit cleanly"
-  assert_include output, "Hello World"
+  assert_false status.success?, "Process did not exit cleanly"
+  assert_include output, "This is not a GitHub repo"
 end
 
-assert('version') do
+assert('It gives you the version') do
   output, status = Open3.capture2(BIN_PATH, "--version")
 
   assert_true status.success?, "Process did not exit cleanly"
